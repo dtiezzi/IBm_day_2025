ed2a95b3ce8d4a614aaebc787ccb005a69b82903
dtiezzi <dtiezzi@usp.br>
2025-05-12 12:34:07 -0300

    suppervised - dt

---
+++ minicurso.py
@@ -0,0 +1,66 @@
+import pandas as pd
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay, classification_report
+from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from scipy.stats import randint
+
+import matplotlib.pyplot as plt
+
+
+exprData = pd.read_csv('data/UCEC_EXPRS_Normalized_filtered.csv')
+
+exprData = exprData.drop('Unnamed: 0', axis=1)
+
+print(exprData.head())
+
+clin = pd.read_csv('data/UCEC_Clinical_522.csv')
+X = exprData
+y = clin['Cluster_3']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+rf = RandomForestClassifier()
+rf.fit(X_train, y_train)
+y_pred = rf.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+
+print("Accuracy:", accuracy)
+print(classification_report(y_test, y_pred))
+
+cm = confusion_matrix(y_test, y_pred)
+
+ConfusionMatrixDisplay(confusion_matrix=cm).plot()
+
+### Ajuste de  hiperparametros
+
+param_dist = {'n_estimators': randint(50,500),
+              'max_depth': randint(1,20)}
+
+
+# Busca aleatória para testar os parâmetros
+rand_search = RandomizedSearchCV(rf, 
+                                 param_distributions = param_dist, 
+                                 n_iter=5, 
+                                 cv=5)
+
+# Trina o modelo
+rand_search.fit(X_train, y_train)
+
+# Variável para o melhor modelo
+best_rf = rand_search.best_estimator_
+
+# Melhores hiperparametros
+print('Best hyperparameters:',  rand_search.best_params_)
+
+# Predição do melhor modelo
+y_pred = best_rf.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+print(classification_report(y_test, y_pred))
+
+# Matriz de confusão
+cm = confusion_matrix(y_test, y_pred)
+ConfusionMatrixDisplay(confusion_matrix=cm).plot()
+plt.show()
